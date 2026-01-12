a67997858e66a984b06fffe1ba4ae2805a0fcb73
Michel <166889728+michelbr84@users.noreply.github.com>
2026-01-12 12:20:00 -0500

    Replace settings.py with corrected constants

--- src/settings.py
+++ src/settings.py
@@ -1,32 +1,22 @@
-# src/settings.py
-SETTINGS = {
-    'SCREEN_WIDTH': 800,
-    'SCREEN_HEIGHT': 600,
-    'TILE_SIZE': 30,
-    'BOARD_WIDTH': 10,
-    'BOARD_HEIGHT': 20,
-    'FPS': 60,
-    'BACKGROUND_COLOR': (0, 0, 0),
-    'COLORS': {
-        'WHITE': (255, 255, 255),
-        'BLACK': (0, 0, 0),
-        'GRAY': (128, 128, 128),
-        'RED': (255, 0, 0),
-        'GREEN': (0, 255, 0),
-        'BLUE': (0, 0, 255),
-        'YELLOW': (255, 255, 0),
-        'CYAN': (0, 255, 255),
-        'MAGENTA': (255, 0, 255),
-        'ORANGE': (255, 165, 0),
-    },
-    'TILE_COLORS': [
-        (0, 0, 0),          # Empty (background)
-        (0, 255, 0),        # I - Green
-        (0, 0, 255),        # J - Blue
-        (255, 255, 0),      # L - Yellow
-        (255, 0, 255),      # O - Magenta
-        (0, 255, 255),      # S - Cyan
-        (255, 0, 0),        # T - Red
-        (255, 165, 0),      # Z - Orange
-    ],
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 600
+CELL_SIZE = 30
+FPS = 60
+BOARD_WIDTH = 10
+BOARD_HEIGHT = 20
+DROP_SPEED = 1000
+PAUSE_KEY = 'p'
+LEFT_KEY = 'left'
+RIGHT_KEY = 'right'
+DOWN_KEY = 'down'
+ROTATE_KEY = 'up'
+
+COLORS = {
+    'I': (0, 255, 255),
+    'O': (255, 255, 0),
+    'T': (128, 0, 128),
+    'S': (0, 255, 0),
+    'Z': (255, 0, 0),
+    'J': (0, 0, 255),
+    'L': (255, 165, 0),
 }
